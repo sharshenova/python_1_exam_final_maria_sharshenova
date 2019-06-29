8853a320614ce81e2563c8db2c73294dfe8f5c48
Maria Sharshenova <maria@sharshenov.com>
2019-06-29 13:58:59 +0600

    Add Book model

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from webapp.models import Author
+from webapp.models import Author, Book
 
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ['pk', 'name', 'birth_date', 'death_date', 'bio']
+    list_display = ['pk', 'name', 'birth_date', 'death_date', 'bio', 'is_deleted']
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'title', 'author', 'published_at', 'file', 'image', 'description']
+
 
 admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -23,3 +23,22 @@ class Author(models.Model):
     class Meta:
         verbose_name = "Автор"
         verbose_name_plural = "Авторы"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=300, verbose_name="Название книги")
+    author = models.ForeignKey(Author, related_name="books", verbose_name="Автор", on_delete=models.CASCADE)
+    published_at = models.CharField(max_length=6, verbose_name="Год издания")
+    file = models.FileField(upload_to='book_files', blank=True, null=True, verbose_name="Файл")
+    image = models.ImageField(upload_to='book_images', blank=True, null=True, verbose_name="Обложка")
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('webapp:book_details', kwargs={'pk': self.pk})
+
+    class Meta:
+        verbose_name = "Книга"
+        verbose_name_plural = "Книги"
