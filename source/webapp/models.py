7b722b4156c76e07c80ed1eb714a92d60aefc66a
Maria Sharshenova <maria@sharshenov.com>
2019-06-29 13:33:01 +0600

    Add field for soft del in Author model

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Author(models.Model):
@@ -7,7 +8,14 @@ class Author(models.Model):
     death_date = models.DateField(null=True, blank=True, verbose_name="Дата смерти")
     bio = models.TextField(max_length=5000, null=True, blank=True, verbose_name="Биография")
     image = models.ImageField(upload_to='author_images', blank=True, null=True, verbose_name="Фото")
+    is_deleted = models.BooleanField(default=False)
 
+    def soft_delete(self):
+        self.is_deleted = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('webapp:author_details', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.name
