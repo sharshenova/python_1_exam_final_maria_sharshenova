73e6b47e5f722127e3d4a384186abfe15915cf93
Maria Sharshenova <maria@sharshenov.com>
2019-06-29 13:31:12 +0600

    Add urls for Author

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import UserDetailView, UserUpdateView
+from .views import UserDetailView, UserUpdateView, AuthorListView, AuthorDetailView, AuthorCreateView, \
+    AuthorUpdateView, AuthorDeleteView
 
 app_name = 'webapp'
 
 urlpatterns = [
     path('user/<int:pk>/', UserDetailView.as_view(), name='user_details'),
     path('user/<int:pk>/update', UserUpdateView.as_view(), name='user_update'),
-
+    path('authors/', AuthorListView.as_view(), name='author_list'),
+    path('authors/<int:pk>/', AuthorDetailView.as_view(), name='author_details'),
+    path('authors/create/', AuthorCreateView.as_view(), name='author_create'),
+    path('authors/<int:pk>/update/', AuthorUpdateView.as_view(), name='author_update'),
+    path('authors/<int:pk>/delete/', AuthorDeleteView.as_view(), name='author_delete'),
 ]
